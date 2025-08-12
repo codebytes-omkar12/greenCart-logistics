10d7bdee5c2ab1ba949894cf52a6a4d146042a11
codebytes-omkar12 <omkarmandal3002@gmail.com>
2025-08-13 02:11:18 +0530

    feat(frontend):Created interfaces to be used in the frontend

---
+++ frontend/src/types.ts
@@ -0,0 +1,40 @@
+export interface IRoute {
+  _id: string;
+  routeID: string;
+  distance: number;
+  trafficLevel: 'Low' | 'Medium' | 'High';
+  baseTime: number;
+}
+
+export interface IDriver {
+  _id: string;
+  name: string;
+  currentShiftHours: number;
+  past7DayWorkHours: number[];
+  isFatigued: boolean;
+}
+
+export interface IOrder {
+  _id: string;
+  orderID: string;
+  value_rs: number;
+  assignedRoute: IRoute;
+  deliveryTimestamp?: string | null;
+}
+
+export interface ISimulation {
+    _id: string;
+    timestamp: string;
+    totalProfit: number;
+    efficiencyScore: number;
+    onTimeDeliveries: number;
+    lateDeliveries: number;
+    tags: string[];
+}
+
+export interface IDashboardStats {
+    totalDrivers: number;
+    totalOrders: number;
+    pendingOrders: number;
+    totalProfit: number;
+}
