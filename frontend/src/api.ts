09556b75010a2a669f2ac26228c4477c616e7f93
codebytes-omkar12 <omkarmandal3002@gmail.com>
2025-08-13 08:10:14 +0530

    final push 2

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { type ICredentials, type ISimulationParams, type IDriver, type IRoute, type IOrder  } from './types';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 
 
 const apiClient = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
