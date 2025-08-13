803f05b5abfadf09d52f9236547aa3a7b5391243
codebytes-omkar12 <omkarmandal3002@gmail.com>
2025-08-13 07:13:32 +0530

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: ['https://cosmic-bavarois-2117da.netlify.app' , 'http://localhost:5173'],
     credentials: true
 }));
 app.use(express.json());
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { type ICredentials, type ISimulationParams, type IDriver, type IRoute, type IOrder  } from './types';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 
 const apiClient = axios.create({
   baseURL: API_URL,
